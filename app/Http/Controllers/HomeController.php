7973d675a47cf6beefb32b57886298f78f9bcd67
PercyTomicha <mspercywillans@gmail.com>
2019-06-06 10:09:00 -0400

    Primer Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use DB;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       // dd($name=Auth::user()->nombre;
+
+       return view('admin.dashboard');
+    }
+}
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout')
+
+@section('contenido')
+ <h1>Has iniciado sesion con exito</h1>
+@stop
